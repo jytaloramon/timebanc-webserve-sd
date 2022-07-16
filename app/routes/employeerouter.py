3e94b5b39a00614bfded11b13c73d7b65180f8ce
Ramon Almeida <ramongoncalves76@gmail.com>
2022-07-15 22:18:51 -0300

    Fix variables names

--- app/routes/employeerouter.py
+++ app/routes/employeerouter.py
@@ -5,34 +5,34 @@ from core.commom.repositoriesinstance import Repositories
 
 from core.domain.employee.usecases import EmployeeUseCases
 
-user_route = Blueprint('employee', __name__, url_prefix='/employee')
+employee_route = Blueprint('employee', __name__, url_prefix='/employee')
 
-user_use_case = EmployeeUseCases(Repositories.employee_repository)
+employee_use_case = EmployeeUseCases(Repositories.employee_repository)
 
 
-@user_route.route('/', methods=['GET', 'POST'])
+@employee_route.route('/', methods=['GET', 'POST'])
 def user_base():
 
     if request.method == 'GET':
-        return {'employees': list(map(lambda x: x.entity_dump(), user_use_case.get_all()))}
+        return {'employees': list(map(lambda x: x.entity_dump(), employee_use_case.get_all()))}
 
     if request.method == 'POST':
         data = request.get_json()
 
         try:
-            return user_use_case.create(
+            return employee_use_case.create(
                 data['name'], data['personal_code'], data['work_code'], data['birth_date']
             ).entity_dump()
         except Conflit as e:
             return {'msg': e.args[0]}, StatusCode.CONFLIT.value
 
 
-@user_route.route('/<int:id>', methods=['GET'])
+@employee_route.route('/<int:id>', methods=['GET'])
 def user_base_id(id: int):
 
     if request.method == 'GET':
 
         try:
-            return user_use_case.get_by_Id(id).entity_dump()
+            return employee_use_case.get_by_Id(id).entity_dump()
         except NotFoundError as e:
             return {'msg': e.args[0]}, StatusCode.NOTFOUND.value
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,10 +1,9 @@
 from flask import Blueprint
 
-from app.routes.userrouter import user_route
+from app.routes.employeerouter import employee_route
 from app.routes.schedulerouter import schedule_router
 
 
 api_routes = Blueprint('api', __name__, url_prefix='/api')
-api_routes.register_blueprint(user_route)
+api_routes.register_blueprint(employee_route)
 api_routes.register_blueprint(schedule_router)
-
